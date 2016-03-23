51c3c4e9019d534aa21e7a8eab16050f2048eae0
Matt Burke <spraints@gmail.com>
2016-03-22 22:28:51 -0400

    Revert "Update rails config for 5.0.0.beta3"
    This reverts commit 51a12e185953e738a6eae7a56223cb08bb0dcda5.

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :marshal, and :hybrid.
+# This is a new Rails 5.0 default, so introduced as a config to ensure apps made with earlier versions of Rails aren't affected when upgrading.
 Rails.application.config.action_dispatch.cookies_serializer = :json
