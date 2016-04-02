3422a59c71be965db2134cfe23a1ee7232254a00
Matt Burke <spraints@gmail.com>
2016-04-01 22:52:43 -0400

    also traffic volume

--- app/controllers/hacky_controller.rb
+++ app/controllers/hacky_controller.rb
@@ -11,6 +11,12 @@ df
 Tables
 ------
 #{`pfctl -vv -s Tables 2>&1`}
+
+=======
+traffic
+-------
+#{`tail -f /var/lib/collectd/csv/zig-or-att/interface-re*/if_{octets,packets}-#{Time.now.strftime("%Y-%m-%d")} 2>&1`}
+
 STAT
   end
 end
